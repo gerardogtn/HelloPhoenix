bcdfb6b5e13a462d6b68b14af19656543d02d3de
Gerardo Teruel <gerardogtn@gmail.com>
2015-10-08 13:41:24 -0500

    :rocket: Add hello route

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,6 +1,10 @@
 defmodule HelloPhoenix.PageController do
   use HelloPhoenix.Web, :controller
 
+  def world(conn, _params) do
+    render conn, "helloworld.html"
+  end
+
   def index(conn, _params) do
     render conn, "index.html"
   end
